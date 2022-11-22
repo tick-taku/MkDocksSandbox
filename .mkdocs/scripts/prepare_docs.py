4eb66834314da6e7844b35b9969bab3b7f20176f
tick-taku <tick-taku@pepabo.com>
2022-11-22 19:20:29 +0900

    Move yml to route

--- .mkdocs/scripts/prepare_docs.py
+++ .mkdocs/scripts/prepare_docs.py
@@ -3,10 +3,13 @@ import shutil
 
 with open('.mkdocs/conf.yml') as file:
   conf = yaml.safe_load(file)
-  print('Move mkdocs.yml to docs')
-  shutil.copy('.mkdocs/mkdocs.yml', 'docs/')
+
+  print('Move mkdocs.yml to route')
+  shutil.copy('.mkdocs/mkdocs.yml', './')
+
   print('Move top md to docs')
   shutil.copy(conf['top'], 'docs/')
+
   for doc in conf['docs']:
     print(f'Move {doc} to docs/')
     shutil.copytree(doc, f'docs/{doc}', dirs_exist_ok=True)
