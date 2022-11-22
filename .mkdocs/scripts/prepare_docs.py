c22cef48224d4d600c599863df0ea50e69bc6456
tick-taku <tick-taku@pepabo.com>
2022-11-22 19:17:16 +0900

    Dump log

--- .mkdocs/scripts/prepare_docs.py
+++ .mkdocs/scripts/prepare_docs.py
@@ -3,7 +3,10 @@ import shutil
 
 with open('.mkdocs/conf.yml') as file:
   conf = yaml.safe_load(file)
+  print('Move mkdocs.yml to docs')
   shutil.copy('.mkdocs/mkdocs.yml', 'docs/')
+  print('Move top md to docs')
   shutil.copy(conf['top'], 'docs/')
   for doc in conf['docs']:
+    print(f'Move {doc} to docs/')
     shutil.copytree(doc, f'docs/{doc}', dirs_exist_ok=True)
