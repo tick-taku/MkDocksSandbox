f2788776547fdb6103005bca576c098dfd3047f2
tick-taku <tick-taku@pepabo.com>
2022-11-23 18:11:58 +0900

    Add logo and favicon

--- .mkdocs/scripts/prepare_docs.py
+++ .mkdocs/scripts/prepare_docs.py
@@ -11,9 +11,20 @@ with open('.mkdocs/conf.yml') as file:
   print('INFO - Moving config file: mkdocs.yml')
   shutil.copy('.mkdocs/mkdocs.yml', './')
 
-  top_md = conf['top']
-  print(f'INFO - Moving top md file: {top_md}')
-  shutil.copy(top_md, 'docs/')
+  if 'top' in conf:
+    top_md = conf['top']
+    print(f'INFO - Moving top md file: {top_md}')
+    shutil.copy(top_md, f'docs/{os.path.basename(top_md)}')
+
+  if 'logo' in conf:
+    logo = conf['logo']
+    print(f'INFO - Moving logo image file: {logo}')
+    shutil.copy(logo, f'docs/{os.path.basename(logo)}')
+
+  if 'favicon' in conf:
+    favicon = conf['favicon']
+    print(f'INFO - Moving favicon image file: {favicon}')
+    shutil.copy(favicon, f'docs/{os.path.basename(favicon)}')
 
   for doc in conf['docs']:
     print(f'INFO - Moving document: {doc}')
