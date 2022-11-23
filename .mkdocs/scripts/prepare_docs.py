cafd710597724d3da9c4ef830ee251c33d3815b7
tick-taku <tick-taku@pepabo.com>
2022-11-23 18:28:48 +0900

    Use const

--- .mkdocs/scripts/prepare_docs.py
+++ .mkdocs/scripts/prepare_docs.py
@@ -2,30 +2,33 @@ import yaml
 import shutil
 import os
 
-print('INFO - mkdir docs')
-os.makedirs('docs/', exist_ok=True)
+PREFERENCE_DIR = '.mkdocs'
+DOCKS_DIR = 'docs'
 
-with open('.mkdocs/conf.yml') as file:
+print('INFO - mkdir docs')
+os.makedirs(DOCKS_DIR, exist_ok=True)
+
+with open(f'{PREFERENCE_DIR}/conf.yml') as file:
   conf = yaml.safe_load(file)
 
   print('INFO - Moving config file: mkdocs.yml')
-  shutil.copy('.mkdocs/mkdocs.yml', './')
+  shutil.copy(f'{PREFERENCE_DIR}/mkdocs.yml', './')
 
   if 'top' in conf:
     top_md = conf['top']
     print(f'INFO - Moving top md file: {top_md}')
-    shutil.copy(top_md, f'docs/{os.path.basename(top_md)}')
+    shutil.copy(top_md, f'{DOCKS_DIR}/{os.path.basename(top_md)}')
 
   if 'logo' in conf:
     logo = conf['logo']
     print(f'INFO - Moving logo image file: {logo}')
-    shutil.copy(logo, f'docs/{os.path.basename(logo)}')
+    shutil.copy(logo, f'{DOCKS_DIR}/{os.path.basename(logo)}')
 
   if 'favicon' in conf:
     favicon = conf['favicon']
     print(f'INFO - Moving favicon image file: {favicon}')
-    shutil.copy(favicon, f'docs/{os.path.basename(favicon)}')
+    shutil.copy(favicon, f'{DOCKS_DIR}/{os.path.basename(favicon)}')
 
   for doc in conf['docs']:
     print(f'INFO - Moving document: {doc}')
-    shutil.copytree(doc, f'docs/{doc}', dirs_exist_ok=True)
+    shutil.copytree(doc, f'{DOCKS_DIR}/{doc}', dirs_exist_ok=True)
