26641174921d90b37e2d5f7025748b39705d90cc
tick-taku <tick-taku@pepabo.com>
2022-12-23 00:49:51 +0900

    Fix post script

--- .mkdocs/scripts/post_update.py
+++ .mkdocs/scripts/post_update.py
@@ -1,28 +1,29 @@
-import datetime
+from datetime import datetime, timedelta
 import os
 import tweepy
 
-DIARY_DIR = 'DailyNotes'
+DIARY_DIR = 'dailynote'
 DIARY_WEB_URL = 'https://tick-taku.com'
 
-def get_todays_diary_path():
-  now = datetime.datetime.now()
-  return f'{now.year}/{now.month}/{now:%Y_%-m_%d}'
+def get_latest_diary_filename(date):
+  return f'{date.year}/{date.month}/{date:%Y_%-m_%d}'
 
-def is_diary_updated(path):
-  return os.path.exists(f'{DIARY_DIR}/{path}.md')
+def is_diary_update(date):
+  latest_diary = get_latest_diary_filename(yesterday)
+  return os.path.exists(f'{DIARY_DIR}/{latest_diary}.md')
 
-def post_tweet(path):
+def post_tweet(date):
   client = tweepy.Client(
     consumer_key = os.environ.get('TWITTER_API_KEY'),
     consumer_secret = os.environ.get('TWITTER_API_SECRET'),
     access_token = os.environ.get('TWITTER_ACCESS_TOKEN'),
     access_token_secret = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
   )
-  message = f'{os.path.basename(path)}\n{DIARY_WEB_URL}/{DIARY_DIR.lower()}/{path}'
+  latest_diary = get_latest_diary_filename(yesterday)
+  message = f'{DIARY_WEB_URL}/{DIARY_DIR.lower()}/{latest_diary}'
   print(client.create_tweet(text = message))
 
-todays_path = get_todays_diary_path()
-if is_diary_updated(todays_path):
+yesterday = datetime.now() - timedelta(1)
+if is_diary_update(yesterday):
   print('INFO - Diary update found.')
-  post_tweet(todays_path)
+  post_tweet(yesterday)
